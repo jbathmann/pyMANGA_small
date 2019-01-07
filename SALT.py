4f080a90cc5944b045cfda199acec5bfa175af28
bathmann <jasper.bathmann@ufz.de>
2019-01-07 14:19:20 +0100

    Documentation for salt.py added, some changes in code style performed

--- SALT.py
+++ SALT.py
@@ -1,34 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Aug  2 15:35:00 2018
-
-@author: bathmann
 
 """
+Class managing the whola salt seltup. This class is containing 3 important ob-
+jects: land, flora and the ogs-project information. The built-in functions are
+designed to manage information transfer between bettina and ogs model
+simulations.
+@date: 2018 - Today
+@author: jasper.bathmann@ufz.de
+"""
+import numpy as np
 import sys
 sys.path.append('./pymeshinteraction/')
 import MeshPointFinder as MPF
 import MeshInteractor
-import numpy as np
 
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 13 14:27:35 2018
-
-@author: bathmann
-"""
 sys.path.append('./pybettina/')
 import Bettina
 import SortFileNames as SFN
-sys.path.append('./pyogsproject/')
 
+sys.path.append('./pyogsproject/')
 import OGSProject
 
+
 class SaltSetup:
-    def __init__(self, name, working_directory, land, flora, constant_density, output_midstring):
+    def __init__(self, name, working_directory, land, flora, constant_density,
+                 output_midstring):
         self.setup_name = name
         self.working_directory = working_directory
         self.boundary_surfaces = []
@@ -38,55 +35,62 @@ class SaltSetup:
         self.constant_density = constant_density
         self.createOGSProject(constant_density)
         self.initializeBettina()
-        
+
     def setLand(self, land):
         self.land = land
-    
+
     def setFlora(self, flora):
         self.flora = flora
-    
+
     def initializeBettina(self):
-        self.bettina = Bettina.Bettina(self.setup_name+"Bettina", self.land, self.flora)
-        
+        self.bettina = Bettina.Bettina(self.setup_name+"Bettina", self.land,
+                                       self.flora)
+
     def createOGSProject(self, constant_density):
-        self.ogsPrj = OGSProject.OGSProject(self.working_directory, self.setup_name + "_OGSproject")
+        self.ogsPrj = OGSProject.OGSProject(self.working_directory,
+                                            self.setup_name + "_OGSproject")
         self.ogsPrj.setLandName(self.land.initial_mesh_name)
         self.ogsPrj.initializeProject(constant_density)
-        self.ogsPrj.project.output_prefix = self.setup_name + self.output_midstring
-        
+        self.ogsPrj.project.output_prefix = (self.setup_name +
+                                             self.output_midstring)
+
     def setInitialConditionNames(self, p_ini, c_ini, q_ini):
         self.c_ini_name = c_ini
         self.p_ini_name = p_ini
         self.q_ini_name = q_ini
         self.ogsPrj.setInitialConditionName(p_ini, c_ini)
-        
+
     def setVariableNames(self, p_var, c_var):
         self.c_var_name = c_var
         self.p_var_name = p_var
         self.ogsPrj.setVariableNames(p_var, c_var)
-        self.land.setCurrentPropertyNames([c_var,p_var])
-        
+        self.land.setCurrentPropertyNames([c_var, p_var])
+
     def setInitialConditionName(self, p_ini, c_ini, q_ini):
         self.c_ini_name = c_ini
         self.p_ini_name = p_ini
         self.q_ini_name = q_ini
         self.ogsPrj.setInitialConditionName(p_ini, c_ini)
-        
+
     def setQiniArray(self, q_ini):
         self.q_ini = q_ini
-        
+
     def addBoundaryConditionForSurfaces(self, variable, *args):
-        self.ogsPrj.createBoundaryConditionsFromList(self.boundary_surfaces, variable, *args)
-        
+        self.ogsPrj.createBoundaryConditionsFromList(self.boundary_surfaces,
+                                                     variable, *args)
+
     def writeOgsProject(self):
         self.ogsPrj.writeProject()
 
     def createBoundarySurface(self, location):
+        # This functions only works for land domains with the shape of regular
+        # 3D boxes.
         land_mesh = self.land.initial_mesh
         land_grid = land_mesh.grid
         origin = [land_mesh.ox, land_mesh.oy, land_mesh.oz]
         deltas = [land_mesh.dx, land_mesh.dy, land_mesh.dz]
-        steps = [land_mesh.extension_points_X, land_mesh.extension_points_Y, land_mesh.extension_points_Z]
+        steps = [land_mesh.extension_points_X, land_mesh.extension_points_Y,
+                 land_mesh.extension_points_Z]
         shift = 0
         if location == "left" or location == "right":
             steps[0] = 1
@@ -98,16 +102,24 @@ class SaltSetup:
             shift = 2
         if location == "back":
             origin[1] = origin[1] + land_mesh.lengthY
-        if location == "bottom" or location == "top": steps[2] = 1
+        if location == "bottom" or location == "top":
+            steps[2] = 1
         if location == "top":
             origin[2] = origin[2] + land_mesh.lengthZ
         boundary_mesh_name = land_mesh.meshName + "_" + location + "Boundary"
         boundary_creator = MPF.MeshPointFinder(land_grid)
-        points, ids = boundary_creator.findPointsOnPlane(steps[0], steps[1], steps[2], deltas[0], deltas[1], deltas[2], origin[0], origin[1], origin[2]) 
+        points, ids = boundary_creator.findPointsOnPlane(steps[0], steps[1],
+                                                         steps[2], deltas[0],
+                                                         deltas[1], deltas[2],
+                                                         origin[0], origin[1],
+                                                         origin[2])
         temppoints = []
         for i in range(len(points)):
             point = points[i]
-            point = point[(0+shift)%3], point[(1+shift)%3], point[(2+shift)%3]
+            x = (0+shift) % 3
+            y = (1+shift) % 3
+            z = (2+shift) % 3
+            point = point[x], point[y], point[z]
             temppoints.append(point)
         boundary = MeshInteractor.MeshInteractor(boundary_mesh_name)
         boundary.CreateMeshFromPoints([points, ids])
@@ -115,68 +127,83 @@ class SaltSetup:
         temp_boundary.CreateMeshFromPoints([temppoints, ids])
         temp_boundary.CreateMultipleTriangles()
         cells = temp_boundary.grid.GetCells()
-        if(land_grid.GetBounds()[-1]-land_grid.GetBounds()[-2]==0):
-            boundary.grid.SetCells(3,cells)
+        if(land_grid.GetBounds()[-1] - land_grid.GetBounds()[-2] == 0):
+            boundary.grid.SetCells(3, cells)
         else:
-            boundary.grid.SetCells(5,cells)
+            boundary.grid.SetCells(5, cells)
         full_c_ini = land_grid.GetPointData().GetArray(self.c_ini_name)
         full_p_ini = land_grid.GetPointData().GetArray(self.p_ini_name)
         full_q_ini = land_grid.GetPointData().GetArray(self.q_ini_name)
-        boundary_c_ini, boundary_p_ini, boundary_q_ini = np.zeros(len(ids)), np.zeros(len(ids)), np.zeros(len(ids))  
+        boundary_c_ini = np.zeros(len(ids))
+        boundary_p_ini = np.zeros(len(ids))
+        boundary_q_ini = np.zeros(len(ids))
         for i in range(len(ids)):
             iD = ids[i]
-            boundary_c_ini[i]=full_c_ini.GetTuple(iD)[0]
-            boundary_p_ini[i]=full_p_ini.GetTuple(iD)[0]
-            boundary_q_ini[i]=full_q_ini.GetTuple(iD)[0]
-        boundary.AddPropertyVector(np.array(boundary_c_ini),self.c_ini_name, "double")
-        boundary.AddPropertyVector(np.array(boundary_p_ini),self.p_ini_name, "double")
-        boundary.AddPropertyVector(np.array(boundary_q_ini),self.q_ini_name, "double")
-        boundary.AddPropertyVector(0,"zero", "double")
+            boundary_c_ini[i] = full_c_ini.GetTuple(iD)[0]
+            boundary_p_ini[i] = full_p_ini.GetTuple(iD)[0]
+            boundary_q_ini[i] = full_q_ini.GetTuple(iD)[0]
+        boundary.AddPropertyVector(np.array(boundary_c_ini), self.c_ini_name,
+                                   "double")
+        boundary.AddPropertyVector(np.array(boundary_p_ini), self.p_ini_name,
+                                   "double")
+        boundary.AddPropertyVector(np.array(boundary_q_ini), self.q_ini_name,
+                                   "double")
+        boundary.AddPropertyVector(0, "zero", "double")
         boundary.OutputMesh(self.working_directory)
         self.boundary_surfaces.append(boundary_mesh_name)
-        
-    def setTimeSteppingAndOutputLoopsForOgs(self,timerepeats, timedeltaTs, outputrepeats, outputdeltaN):
-        self.ogsPrj.setTimeSteppingAndOutputLoops(timerepeats, timedeltaTs, outputrepeats, outputdeltaN)
+
+    def setTimeSteppingAndOutputLoopsForOgs(self, timerepeats, timedeltaTs,
+                                            outputrepeats, outputdeltaN):
+        self.ogsPrj.setTimeSteppingAndOutputLoops(timerepeats, timedeltaTs,
+                                                  outputrepeats, outputdeltaN)
 
     def updateFloraBoundaryConditions(self):
         self.root_surfaces = self.flora.getAllRootNames()
-        args = "NonuniformVariableDependentNeumann", "constant", "coeff1", "coeff2", "coeff3"
-        self.ogsPrj.createTreeBoundaryConditionsFromList(self.root_surfaces, "pressure", *args)
-    
+        c1, c2, c3 = "coeff1", "coeff2", "coeff3"
+        args = "NonuniformVariableDependentNeumann", "constant", c1, c2, c3
+        self.ogsPrj.createTreeBoundaryConditionsFromList(self.root_surfaces,
+                                                         "pressure", *args)
+
     def updateBoundaryConditions(self):
         self.ogsPrj.resetBoundaryConditions()
         self.addBoundaryConditionForSurfaces("pressure")
         self.addBoundaryConditionForSurfaces("concentration")
         self.updateFloraBoundaryConditions()
-        
+
     def runOGS(self):
         self.ogsPrj.runOgs()
-        
+
     def setOgsTiniTend(self, t_ini, t_end):
         self.ogsPrj.setTiniTend(t_ini, t_end)
-        
+
     def progressBettina(self, files_t):
-        self.bettina.setVariableSubsurfaceProperties(files_t, self.working_directory)
+        self.bettina.setVariableSubsurfaceProperties(files_t,
+                                                     self.working_directory)
         self.bettina.evolveSystem()
-        
+
     def updateModel(self):
         self.ogsPrj.setLandName(self.land.initial_mesh_name)
         self.ogsPrj.initializeProject(self.constant_density)
-        self.ogsPrj.project.output_prefix = self.setup_name + self.output_midstring
+        self.ogsPrj.project.output_prefix = self.setup_name + \
+            self.output_midstring
 
     def createMeshCollection(self, prefix, postfix):
-        self.file_reader_meshes = SFN.ReadAndSortFileNames(self.working_directory, prefix, postfix)
-        
+        self.file_reader_meshes = SFN.ReadAndSortFileNames(
+                self.working_directory, prefix, postfix)
+
     def createFloraCollection(self, prefix, postfix):
-        self.file_reader_flora = SFN.ReadAndSortFileNames(self.working_directory, prefix, postfix)
+        self.file_reader_flora = SFN.ReadAndSortFileNames(
+                self.working_directory, prefix, postfix)
 
     def updateFloraCollection(self):
-        self.file_reader_flora.createPvDFile(self.setup_name + "_flora_meshes" + ".pvd")
+        self.file_reader_flora.createPvDFile(
+                self.setup_name + "_flora_meshes" + ".pvd")
         files = self.file_reader_flora.getSortedFiles()
         self.file_reader_flora.addMeshesToPvdFile(files)
 
     def updateMeshCollection(self):
-        self.file_reader_meshes.createPvDFile(self.setup_name + "_land_meshes"+ ".pvd")
+        self.file_reader_meshes.createPvDFile(
+                self.setup_name + "_land_meshes" + ".pvd")
         files = self.file_reader_meshes.getSortedFiles()
         rm_files = []
         for file in files:
@@ -189,32 +216,32 @@ class SaltSetup:
     def createTreeCollection(self, species_list, postfix):
         self.file_reader_trees_list = []
         for species in species_list:
-            self.file_reader_trees_list.append(SFN.ReadAndSortFileNames(self.working_directory, species, postfix))
+            self.file_reader_trees_list.append(
+                SFN.ReadAndSortFileNames(self.working_directory,
+                                         species, postfix))
 
     def updateTreeCollection(self):
         i = 1
         for file_reader_trees in self.file_reader_trees_list:
-            
             file_reader_trees.createPvDFile(
-                    self.setup_name + "_tree_meshes_"+ file_reader_trees.prefix 
-                    + ".pvd") 
-            i+=1
+                self.setup_name + "_tree_meshes_" + file_reader_trees.prefix
+                + ".pvd")
+            i += 1
             files = file_reader_trees.getSortedFiles()
             file_reader_trees.addMeshesToPvdFile(files)
-        
-    def readAndPassTMeshFileNames(self,t_ini, t_end):
+
+    def readAndPassTMeshFileNames(self, t_ini, t_end):
         t_files = self.file_reader_meshes.getFilesInTimeIntervall(t_ini, t_end)
         self.progressBettina(t_files)
         self.updateMeshCollection()
         self.updateTreeCollection()
         self.updateFloraCollection()
-    
-    def setAndRunOgs(self, t_ini, t_end, timerepeats,timedeltaTs,
+
+    def setAndRunOgs(self, t_ini, t_end, timerepeats, timedeltaTs,
                      outputrepeats, outputdeltaN):
         self.setOgsTiniTend(t_ini, t_end)
-        self.setTimeSteppingAndOutputLoopsForOgs(timerepeats, timedeltaTs, outputrepeats, outputdeltaN)
-    
+        self.setTimeSteppingAndOutputLoopsForOgs(timerepeats, timedeltaTs,
+                                                 outputrepeats, outputdeltaN)
+
         self.writeOgsProject()
         self.runOGS()
-        
-        
--- SortFileNames.py
+++ SortFileNames.py
@@ -5,28 +5,30 @@ Tool to identify specific files in a folder.
 working_directory: path to folder, where the files are stored
 prefix: characteristic string contained in all files of interest
 postfix: file ending, e.g. ".vtu", ".pvd"...
-@author: bathmann
+@date: 2018-Today
+@author: jasper.bathmann@ufz.de
 """
 import os
 import re
+
 
 class ReadAndSortFileNames:
     def __init__(self, working_directory, prefix, postfix):
         self.working_directory = working_directory
         self.prefix = prefix
-        self.postfix = postfix    
+        self.postfix = postfix
         self.numbers = re.compile(r'(\d+)')
-    
+
     def numericalSort(self, value):
-        #function to sort given list of strings numerically, e.g. "file1", 
-        #"file2",...
+        # function to sort given list of strings numerically, e.g. "file1",
+        # "file2",...
         parts = self.numbers.split(value)
         parts[1::2] = map(int, parts[1::2])
         return parts
-    
+
     def getSortedFiles(self):
-        #returns all filenames containing the prefix and the postfix numerical
-        #sorted 
+        # returns all filenames containing the prefix and the postfix numerical
+        # sorted
         relevant_files = []
         files = (os.listdir(self.working_directory))
         for file in files:
@@ -34,11 +36,11 @@ class ReadAndSortFileNames:
                 relevant_files.append(file)
         sorted_relevant_files = sorted(relevant_files, key=self.numericalSort)
         return sorted_relevant_files
-    
+
     def getFilesInTimeIntervall(self, t_begin, t_end):
-        #returns all filenames containing the prefix and the postfix and a 
-        #number large than t_begin and smaller than t_end in numerical
-        #sorted order
+        # returns all filenames containing the prefix and the postfix and a
+        # number large than t_begin and smaller than t_end in numerical
+        # sorted order
         relevant_files = []
         files = self.getSortedFiles()
         for file in files:
@@ -50,7 +52,7 @@ class ReadAndSortFileNames:
         return relevant_files
 
     def createPvDFile(self, pvd_file_name):
-        #creates an empty pvd file with given pvd_file_name
+        # creates an empty pvd file with given pvd_file_name
         self.pvd_file_name = self.working_directory + pvd_file_name
         file = open(self.pvd_file_name, "w")
         file.write("""<?xml version="1.0"?>
@@ -58,13 +60,13 @@ class ReadAndSortFileNames:
   <Collection>
                    \n""")
         file.close()
-        
+
     def addMeshesToPvdFile(self, meshes):
-        #adds a list of mesh files to the empty pvd file created in 
-        #self.createPvDFile
+        # adds a list of mesh files to the empty pvd file created in
+        # self.createPvDFile
         tempfile = open(self.pvd_file_name, "r")
         lines = tempfile.readlines()
-        if(len(lines)>4):
+        if(len(lines) > 4):
             lines = lines[:-2]
         tempfile.close()
         file = open(self.pvd_file_name, "w")
@@ -73,13 +75,14 @@ class ReadAndSortFileNames:
             file.write(line)
         for mesh in meshes:
             t = float(mesh.strip(".vtu").split("_t_")[-1])
-            file.write('        <DataSet timestep="' + str(t) + '" group="" part="0" file="'+ mesh + '"/>\n')
+            file.write('        <DataSet timestep="' + str(t) +
+                       '" group="" part="0" file="' + mesh + '"/>\n')
         file.write("""  </Collection>
 </VTKFile>""")
         file.close()
-        
+
     def finishPvDFile(self):
-        #adds the closing lines to a given pvd file.
+        # adds the closing lines to a given pvd file.
         file = open(self.pvd_file_name, "a")
         file.write("""  </Collection>
 </VTKFile>""")
