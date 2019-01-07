b94e5dcb82343cf8a0c612a91f79648eb26406e1
bathmann <jasper.bathmann@ufz.de>
2019-01-07 12:18:43 +0100

    Documentation for sortfilenames added

--- SortFileNames.py
+++ SortFileNames.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov 14 08:38:51 2018
-
+Tool to identify specific files in a folder.
+working_directory: path to folder, where the files are stored
+prefix: characteristic string contained in all files of interest
+postfix: file ending, e.g. ".vtu", ".pvd"...
 @author: bathmann
 """
 import os
@@ -16,11 +18,15 @@ class ReadAndSortFileNames:
         self.numbers = re.compile(r'(\d+)')
     
     def numericalSort(self, value):
+        #function to sort given list of strings numerically, e.g. "file1", 
+        #"file2",...
         parts = self.numbers.split(value)
         parts[1::2] = map(int, parts[1::2])
         return parts
     
     def getSortedFiles(self):
+        #returns all filenames containing the prefix and the postfix numerical
+        #sorted 
         relevant_files = []
         files = (os.listdir(self.working_directory))
         for file in files:
@@ -30,6 +36,9 @@ class ReadAndSortFileNames:
         return sorted_relevant_files
     
     def getFilesInTimeIntervall(self, t_begin, t_end):
+        #returns all filenames containing the prefix and the postfix and a 
+        #number large than t_begin and smaller than t_end in numerical
+        #sorted order
         relevant_files = []
         files = self.getSortedFiles()
         for file in files:
@@ -41,6 +50,7 @@ class ReadAndSortFileNames:
         return relevant_files
 
     def createPvDFile(self, pvd_file_name):
+        #creates an empty pvd file with given pvd_file_name
         self.pvd_file_name = self.working_directory + pvd_file_name
         file = open(self.pvd_file_name, "w")
         file.write("""<?xml version="1.0"?>
@@ -50,6 +60,8 @@ class ReadAndSortFileNames:
         file.close()
         
     def addMeshesToPvdFile(self, meshes):
+        #adds a list of mesh files to the empty pvd file created in 
+        #self.createPvDFile
         tempfile = open(self.pvd_file_name, "r")
         lines = tempfile.readlines()
         if(len(lines)>4):
@@ -67,6 +79,7 @@ class ReadAndSortFileNames:
         file.close()
         
     def finishPvDFile(self):
+        #adds the closing lines to a given pvd file.
         file = open(self.pvd_file_name, "a")
         file.write("""  </Collection>
 </VTKFile>""")
