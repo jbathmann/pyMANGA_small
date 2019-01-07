b0e204f3138592ead48456718d64cc46727d3d7d
bathmann <jasper.bathmann@ufz.de>
2019-01-07 14:38:29 +0100

    changes to improve code style

--- 2trees_example.py
+++ 2trees_example.py
@@ -1,131 +1,143 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov 20 12:21:17 2018
-
-@author: bathmann
+Example script. This file passes parameters to the salt model, such that 2
+trees with one of them downstream to the other are located on a 3 dimensional
+land domain.
+@date: 2018-Today
+@author: jasper.bathmann@ufz.de
 """
+import ExecuteStandardModelSetupWithGivenParameters
+# ExecuteStandardModelSetupWithGivenParameters.Run(args) starts the salt model
+# with the configuration given below
 
+# -- Parameters on local working directories and setup naming ###
 
-### Parameters on local working directories and setup naming ###
-
-#working_directory: Directory, where simulation results are saved. Please make
-#sure the location exists on your local machine
+# working_directory: Directory, where simulation results are saved. Please make
+# sure the location exists on your local machine
 working_directory = "./testcases/2trees/"
-#setup_name: This string is contained in all output files generated
+# setup_name: This string is contained in all output files generated
 setup_name = "2trees"
-#land_name: specific name, which is contained on all output files for the 
-#meshes representing the land domain
+# land_name: specific name, which is contained on all output files for the
+# meshes representing the land domain
 land_name = "_exampleland"
-#flora_name: specific name, which is contained on all output files for the 
-#meshes associated with the flora
+# flora_name: specific name, which is contained on all output files for the
+# meshes associated with the flora
 flora_name = "_exampleflora"
-#output_midstring: specific name, which is contained on all output files for the 
-#meshes associated with the ogs output
+# output_midstring: specific name, which is contained on all output files for
+# the meshes associated with the ogs output
 output_midstring = "_ogsOutput"
-#postfix_vtu_files: specific string, which is at the end of all vtu files
-#saved on within the working directory
+# postfix_vtu_files: specific string, which is at the end of all vtu files
+# saved on within the working directory
 postfix_vtu_files = ".vtu"
 
 
-### Parameters on land domain properties ###
+# -- Parameters on land domain properties ###
 
-## Geometry ##
-#land_length_x,y,z: the dimensions (in m) of the simulated land domain
+# - Geometry ##
+# land_length_x,y,z: the dimensions (in m) of the simulated land domain
 land_length_x, land_length_y, land_length_z = 10, 3, 3
-#land_origin_x,y: the coordinates of the upper-left-bottom corner of the land
-#mesh. The z_coordinate of the upper_left_bottom corner is always set to 
-#z = -land_length_z
+# land_origin_x,y: the coordinates of the upper-left-bottom corner of the land
+# mesh. The z_coordinate of the upper_left_bottom corner is always set to
+# z = -land_length_z
 land_origin_x, land_origin_y = 0, 0
-#land_layers_x,y,z: the number of layers in each dimension.
+# land_layers_x,y,z: the number of layers in each dimension.
 land_layers_x, land_layers_y, land_layers_z = 100, 30, 1
 
-## Names for primary variables in subsurface processes ##
-#pressure_variable_name, concentration_variable_name: Names of the primary 
-#variables themselves
-pressure_variable_name, concentration_variable_name = "pressure", "concentration"
-#pressure_initial_name, concentration_initial_name: names for the initial con-
-#ditions for the primary variables
+# #  Names for primary variables in subsurface processes ##
+# pressure_variable_name, concentration_variable_name: Names of the primary
+# variables themselves
+pressure_variable_name, concentration_variable_name = \
+                                                "pressure", "concentration"
+# pressure_initial_name, concentration_initial_name: names for the initial con-
+# ditions for the primary variables
 pressure_initial_name, concentration_initial_name = "p_ini", "c_ini"
-#darcy_velocity_initial_name: name of the initial darcy velocity distribution. 
-#Necessary to define 2nd type boundary conditions
+# darcy_velocity_initial_name: name of the initial darcy velocity distribution.
+# Necessary to define 2nd type boundary conditions
 darcy_velocity_initial_name = "q_ini"
-#nade_id_name: name of the property vector containing the node ids. Necessary
-#for ogs to run properly.
+# nade_id_name: name of the property vector containing the node ids. Necessary
+# for ogs to run properly.
 node_id_name = "bulk_node_ids"
-#ini_darcy/pressure/concentration_function(point): functions to create intial 
-#conditions. Note: all functions must depend on point, which is a (3,1) tuple
-#in order to allow for spatially depending initial distributions
+# ini_darcy/pressure/concentration_function(point): functions to create intial
+# conditions. Note: all functions must depend on point, which is a (3,1) tuple
+# in order to allow for spatially depending initial distributions
 dp_dx = 1e-3
-def ini_darcy_function(point,dp_dx = dp_dx):
+
+
+def ini_darcy_function(point, dp_dx=dp_dx):
     return -1000*9.81*dp_dx*1.239*1e-5
-def ini_pressure_function(point, dp_dx = dp_dx):
+
+
+def ini_pressure_function(point, dp_dx=dp_dx):
     return -1000*9.81*(point[2]+dp_dx*point[0])
+
+
 def ini_concentration_function(point):
     return 0.035
 
 
-### Parameters on flora properties ###
-    
-#tree species: list containing the species which considered in the initial
-#tree distribution. Given in the form: [species1, species2, ...], with speciesi
-#being a string with the species name
+# ## Parameters on flora properties ###
+
+# tree species: list containing the species which considered in the initial
+# tree distribution. Given in the form: [species1, species2, ...], with
+# species_i being a string with the species name
 tree_species = ["Avicennia"]
-#initial_plants: number of individums planted for each species for the initial
-#plant distributions. Given in the form [n_species1, n_species2, ...], with
-#n_speciesi being the number of individuums of each species
+# initial_plants: number of individums planted for each species for the initial
+# plant distributions. Given in the form [n_species1, n_species2, ...], with
+# n_speciesi being the number of individuums of each species
 initial_plants = [150]
-#flora_plant_function: function defining how the initial plant distribution has
-#to look like
+# flora_plant_function: function defining how the initial plant distribution
+# has to look like
+
+
 def flora_plant_function(flora, land):
     import Tree
-    new_tree = Tree.Tree(2.5, 1.5, flora.land, "Avicennia", 0, 
+    new_tree = Tree.Tree(2.5, 1.5, flora.land, "Avicennia", 0,
                          flora.flora_name)
     new_tree.plantTree(flora.working_directory)
     flora.trees.append(new_tree)
-    
-    second_tree = Tree.Tree(5, 1.5, flora.land, "Avicennia", 1, 
+
+    second_tree = Tree.Tree(5, 1.5, flora.land, "Avicennia", 1,
                             flora.flora_name)
     second_tree.plantTree(flora.working_directory)
     flora.trees.append(second_tree)
 
 
-### Parameters on time loop ###
-#bettina_timesteps: length of one timestep in bettina in [s]. Note: half a year 
-#corresponds to 15778800.0 seconds
+# ## Parameters on time loop ###
+# bettina_timesteps: length of one timestep in bettina in [s]. Note: half a
+# year corresponds to 15778800.0 seconds
 bettina_delta_t = 15778800.0/(6.)
-#number_of_bettina_timesteps: total number of iterations of the bettina model
+# number_of_bettina_timesteps: total number of iterations of the bettina model
 number_of_bettina_timesteps = 50 * 12
-#ogs_timerepeats
-#ogs_time_delta_ts: list containing different timestep lengths in s for the ogs 
-#simulation
-ogs_time_delta_ts = [1e-1, 1e0, 60, 300, 900, 1800, 3600, 3600*2, 3600*4,3600*8]
-#ogs_timerepeats: list of the same length as ogs_time_delta_ts containing the 
-#number of iterations with the different step sizes given in ogs_time_delta_ts
+# ogs_timerepeats
+# ogs_time_delta_ts: list containing different timestep lengths in s for the
+# ogs simulation
+ogs_time_delta_ts = [1e-1, 1e0, 60, 300, 900, 1800,
+                     3600, 3600*2, 3600*4, 3600*8]
+# ogs_timerepeats: list of the same length as ogs_time_delta_ts containing the
+# number of iterations with the different step sizes given in ogs_time_delta_ts
 ogs_timerepeats = [1,   1,  10, 10,  10,  50,   100,   100,     100,  1000]
-#ogs_outputdeltaN: list containing number of steps for after which ogs is
-#writing an output file
+# ogs_outputdeltaN: list containing number of steps for after which ogs is
+# writing an output file
 ogs_outputdeltaN = [10000]
-#ogs_outputrepeats: list of same shape as ogs_outputdeltaN containing the 
-#number of iterations with the different output intervals given in 
-#ogs_outputdeltaN
-ogs_outputrepeats =[1]
+# ogs_outputrepeats: list of same shape as ogs_outputdeltaN containing the
+# number of iterations with the different output intervals given in
+# ogs_outputdeltaN
+ogs_outputrepeats = [1]
 
 
-### Salt model execution file import and run ###
-#ExecuteStandardModelSetupWithGivenParameters.Run(args) starts the salt model 
-#with the configuration given above
-import ExecuteStandardModelSetupWithGivenParameters
-ExecuteStandardModelSetupWithGivenParameters.Run(working_directory, setup_name, 
-                 land_name, flora_name, output_midstring, postfix_vtu_files, 
-                 land_length_x, land_length_y, land_length_z, land_origin_x, 
-                 land_origin_y, land_layers_x, land_layers_y, land_layers_z, 
-                 pressure_variable_name, concentration_variable_name, 
-                 pressure_initial_name, concentration_initial_name, 
-                 darcy_velocity_initial_name, node_id_name, ini_darcy_function,
-                 ini_pressure_function, ini_concentration_function,
-                 tree_species, initial_plants, flora_plant_function, 
-                 bettina_delta_t, number_of_bettina_timesteps, 
-                 ogs_time_delta_ts, ogs_timerepeats, ogs_outputdeltaN,
-                 ogs_outputrepeats)
-    
+# # #   Salt model execution file import and run # # #
+
+ExecuteStandardModelSetupWithGivenParameters.Run(
+        working_directory, setup_name,
+        land_name, flora_name, output_midstring, postfix_vtu_files,
+        land_length_x, land_length_y, land_length_z, land_origin_x,
+        land_origin_y, land_layers_x, land_layers_y, land_layers_z,
+        pressure_variable_name, concentration_variable_name,
+        pressure_initial_name, concentration_initial_name,
+        darcy_velocity_initial_name, node_id_name, ini_darcy_function,
+        ini_pressure_function, ini_concentration_function,
+        tree_species, initial_plants, flora_plant_function,
+        bettina_delta_t, number_of_bettina_timesteps,
+        ogs_time_delta_ts, ogs_timerepeats, ogs_outputdeltaN,
+        ogs_outputrepeats)
--- OGSProject.py
+++ OGSProject.py
@@ -8,97 +8,118 @@ project_name: project file name
 """
 import pyOgsProject
 import os
+
+
 class OGSProject:
-    def __init__(self,project_dir, project_name):
+    def __init__(self, project_dir, project_name):
         self.project_dir = project_dir
         self.project_name = project_name
         self.g = 9.81
         self.c_ini_name = "c_ini"
         self.p_ini_name = "p_ini"
         self.q_ini_name = "q_ini"
-        
+
     def setProjectdir(self, project_dir):
         self.project_dir = project_dir
-    
+
     def setProjectName(self, project_name):
         self.project_name = project_name
-        
+
     def setLandName(self, land_name):
         self.land_name = land_name
 
     def setGravitation(self, value):
         self.g = value
-        
+
     def setInitialConditionName(self, p_ini, c_ini):
         self.c_ini_name = c_ini
         self.p_ini_name = p_ini
-        self.project.resetInitialConditions(self.p_ini_name,self.c_ini_name)
-        
+        self.project.resetInitialConditions(self.p_ini_name, self.c_ini_name)
+
     def getInitialConditionName(self):
         return self.c_ini_name, self.p_ini_name
-    
+
     def setVariableNames(self, p_var, c_var):
         self.c_var_name = c_var
         self.p_var_name = p_var
-        
+
     def getVariableNames(self):
         return self.c_var_name, self.p_var_name
-    
+
     def iniParameters(self):
         self.parameter_names = []
         self.parameter_types = []
         self.parameter_values = []
-        
+
     def addParameter(self, name, tYpe, value):
         self.parameter_names.append(name)
         self.parameter_types.append(tYpe)
         self.parameter_values.append(value)
 
     def initializeProject(self, constant_density):
-        self.project = pyOgsProject.GenerateProject(self.project_dir+self.project_name + ".prj")
+        self.project = pyOgsProject.GenerateProject(self.project_dir +
+                                                    self.project_name + ".prj")
         self.project.setMesh(self.land_name+".vtu")
         self.project.setStandardProcessInformation()
         self.project.setStandartTimeLoop()
         self.project.setStandartParameters()
         self.project.setStandardDensityModel()
-        if constant_density: self.project.densityModel = "Constant"
+        if constant_density:
+            self.project.densityModel = "Constant"
         self.project.setStandardNonlinearSolvers()
         self.project.resetInitialConditions(self.p_ini_name, self.c_ini_name)
         self.project.processspeci_bo_force = "0 0 -"+str(self.g)
         self.project.resetBoundaryConditions()
-        
+
     def writeProject(self):
         self.project.writeProjectFile()
-        
+
     def createBoundaryConditionsFromList(self, mesh_list, variable):
         for mesh in mesh_list:
             if "right" in mesh:
                 if variable == self.c_var_name:
-                    #"constant", "coeff1", "coeff2", "coeff3"
-                    #"zero", "self,q_ini_name", "zero", "zero"
-                    self.project.createBoundaryCondition("c", "NonuniformVariableDependentNeumann", mesh, "zero", self.q_ini_name, "zero", "zero")
+                    # "constant", "coeff1", "coeff2", "coeff3"
+                    # "zero", "self,q_ini_name", "zero", "zero"
+                    self.project.createBoundaryCondition(
+                            "c", "NonuniformVariableDependentNeumann", mesh,
+                            "zero", self.q_ini_name, "zero", "zero")
                 if variable == self.p_var_name:
-                    self.project.createBoundaryCondition("p", "NonuniformNeumann", "geometry","right",mesh, self.q_ini_name)
+                    self.project.createBoundaryCondition(
+                            "p", "NonuniformNeumann", "geometry", "right",
+                            mesh, self.q_ini_name)
             else:
                 if variable == self.c_var_name:
-                    self.project.createBoundaryCondition("c", "NonuniformDirichlet", "geometry","right",mesh, self.c_ini_name)
+                    self.project.createBoundaryCondition(
+                            "c", "NonuniformDirichlet", "geometry", "right",
+                            mesh, self.c_ini_name)
                 if variable == self.p_var_name:
-                    self.project.createBoundaryCondition("p", "NonuniformDirichlet", "geometry","right",mesh, self.p_ini_name)
-    def createTreeBoundaryConditionsFromList(self, mesh_list, variable, tYpe, constant, coeff1, coeff2, coeff3):             
+                    self.project.createBoundaryCondition(
+                            "p", "NonuniformDirichlet", "geometry", "right",
+                            mesh, self.p_ini_name)
+
+    def createTreeBoundaryConditionsFromList(
+            self, mesh_list, variable, tYpe, constant, coeff1, coeff2, coeff3):
         for mesh in mesh_list:
             if variable == self.c_var_name:
-                self.project.createBoundaryCondition("c",tYpe, mesh, constant, coeff1, coeff2, coeff3)
+                self.project.createBoundaryCondition(
+                        "c", tYpe, mesh, constant, coeff1, coeff2, coeff3)
             if variable == self.p_var_name:
-                self.project.createBoundaryCondition("p",tYpe, mesh, constant, coeff1, coeff2, coeff3)
-    def setTimeSteppingAndOutputLoops(self, timerepeats, timedeltaTs, outputrepeats, outputdeltaN):
-        self.project.setTimeSteppingAndOutputLoops(timerepeats, timedeltaTs, outputrepeats, outputdeltaN)
-   
+                self.project.createBoundaryCondition(
+                        "p", tYpe, mesh, constant, coeff1, coeff2, coeff3)
+
+    def setTimeSteppingAndOutputLoops(
+            self, timerepeats, timedeltaTs, outputrepeats, outputdeltaN):
+        self.project.setTimeSteppingAndOutputLoops(timerepeats, timedeltaTs,
+                                                   outputrepeats, outputdeltaN)
+
     def resetBoundaryConditions(self):
         self.project.resetBoundaryConditions()
-    
+
     def setTiniTend(self, t_ini, t_end):
         self.project.time_stepping_t_ini = str(t_ini)
         self.project.time_stepping_t_end = str(t_end)
-    
+
     def runOgs(self):
-        os.system("ogs "+ self.project_dir + self.project_name + ".prj -o " + self.project_dir )
+        os.system(
+                "ogs " + self.project_dir + self.project_name +
+                ".prj -o " + self.project_dir)
