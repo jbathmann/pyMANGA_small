383e85902d8cf44c3db591c5a0afd367f1e83629
bathmann <jasper.bathmann@ufz.de>
2019-04-15 18:32:01 +0200

    2trees_example adapted for paper tests

--- 2trees_example.py
+++ 2trees_example.py
@@ -38,13 +38,13 @@ postfix_vtu_files = ".vtu"
 # - Geometry ##
 # land_length_x,y,z: the dimensions (in m) of the simulated land domain
 # -- type:float
-land_length_x, land_length_y, land_length_z = 10, 6, 3
+land_length_x, land_length_y, land_length_z = 12, 6, 5
 # land_origin_x,y: the coordinates of the upper-left-bottom corner of the land
 # mesh. The z_coordinate of the upper_left_bottom corner is always set to
 # z = -land_length_z -- type:float
 land_origin_x, land_origin_y = 0, 0
 # land_layers_x,y,z: the number of layers in each dimension. -- type:int
-land_layers_x, land_layers_y, land_layers_z = 40, 24, 2
+land_layers_x, land_layers_y, land_layers_z = 72, 36, 25
 
 # - Names for primary variables in subsurface processes ##
 # pressure_variable_name, concentration_variable_name: Names of the primary
@@ -68,11 +68,11 @@ dp_dx = 1e-3
 
 
 def ini_darcy_function(point, dp_dx=dp_dx):
-    return -1000*9.81*dp_dx*1.239*1e-5
+    return -1000*(1+.701*.035)*9.81*dp_dx*1.239*1e-5
 
 
 def ini_pressure_function(point, dp_dx=dp_dx):
-    return -1000*9.81*(point[2]+dp_dx*point[0])
+    return -1000*(1+.701*.035)*9.81*(point[2]+dp_dx*point[0])
 
 
 def ini_concentration_function(point):
@@ -96,12 +96,12 @@ initial_plants = [150]
 
 def flora_plant_function(flora, land):
     import Tree
-    new_tree = Tree.Tree(2.5, 3, flora.land, "Avicennia", 0,
+    new_tree = Tree.Tree(2., 2., flora.land, "Avicennia", 0,
                          flora.flora_name)
     new_tree.plantTree(flora.working_directory)
     flora.trees.append(new_tree)
 
-    second_tree = Tree.Tree(5, 3, flora.land, "Avicennia", 1,
+    second_tree = Tree.Tree(4, 2, flora.land, "Avicennia", 1,
                             flora.flora_name)
     second_tree.plantTree(flora.working_directory)
     flora.trees.append(second_tree)
@@ -122,7 +122,7 @@ ogs_fixed_output_times = [60 * 60 * 24 * 5, 60 * 60 * 24 * 10,
                           60 * 60 * 24 * 25, 60 * 60 * 24 * 30]
 # ogs_outputdeltaN: list containing number of steps for after which ogs is
 # writing an output file -- type:list of ints
-ogs_outputdeltaN = [1000]
+ogs_outputdeltaN = [500]
 # ogs_outputrepeats: list of same shape as ogs_outputdeltaN containing the
 # number of iterations with the different output intervals given in
 # ogs_outputdeltaN -- type:list of ints
